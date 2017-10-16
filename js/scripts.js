45b22209afee1fe9cbd08d573d9d331496c19830
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-10-16 22:28:46 +0200

    poprawki ale nie działa

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+$(document).ready(function(){
 function Button(text) {
 	this.text = text || 'Hello';
 }
+Button.prototype = {
 create: function() {
 	var self = this;
 	this.$element = $('<button>');
@@ -8,8 +10,13 @@ create: function() {
 	this.$element.click(function() {
 		alert(self.text);
 	});
-	
-	$('body').append(this.$element);
+}	
 }
+
+$('body').append(this.$element);
+
 var btn1 = new Button('Hello!');
+
 btn1.create();
+
+});
