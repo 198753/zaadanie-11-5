27cea957bf7531cdbb4b8f67b73f248e725a7833
Tomasz Andrzejewski <kasta.tomek@wp.pl>
2017-10-17 15:19:27 +0200

    poprawka

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-function Button(text) {
-	this.text = text || 'Hello';
-}
-Button.prototype = {
-create: function() {
-	var self = this;
-	this.$element = $('<button>');
-	this.$element.text(this.text);
-	this.$element.click(function() {
-		alert(self.text);
-	});
-$('body').append(this.$element);
-}	
-}
+	function Button(text) {
+		this.text = text || 'Hello';
+	}
+	Button.prototype = {
+		create: function() {
+			var self = this;
+			this.$element = $('<button>');
+			this.$element.text(this.text);
+			this.$element.click(function() {
+				alert(self.text);
+			});
+			$('body').append(this.$element);
+		}	
+	}
 
-var btn1 = new Button('Hello!');
+	var btn1 = new Button('Hello!');
 
-btn1.create();
+	btn1.create();
 
 });
